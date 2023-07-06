008ccd4664106175dfae725aee9f34f8afd5fe84
rohan future devoloper <xrohaislam373@gmail.com>
2023-07-07 02:41:25 +0600

    api

---
+++ src/pages/NotFund404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFund404 = () => {
+    return (
+        <div>
+            <h1>NotFund404</h1>
+        </div>
+    );
+};
+
+export default NotFund404;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,18 +1,43 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
+import Category from "../pages/Home/Category/category";
+import NewsLayout from "../layouts/NewsLayout";
+import News from "../pages/Home/News/News/News";
+import NotFund404 from "../pages/NotFund404";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/category/:id",
+        element: <Category></Category>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/categories/${params.id}`),
+      },
+      {
+        path: "*",
+        element: <NotFund404></NotFund404>,
+      },
+    ],
+  },
+
+  {
+    path: "news",
+    element: <NewsLayout></NewsLayout>,
+    children: [
+      {
+        path: ":id",
+        element: <News></News>,
+      },
+    ],
+  },
+]);
 
 export default router;
